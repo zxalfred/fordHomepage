1a239e4334b3daf417c4ad879b988ff5c5e42dc4
Alfred <alfred.zhao@dragonsoftbravo.com>
2018-09-08 14:38:03 +0800

    add carousel

---
+++ js/billboardCarousel.js
@@ -0,0 +1,11 @@
+// billboardCarousel.js
+// Author: Alfred
+// 2018-09-08
+
+{
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: $(".carousel-controls"),
+		customDirectionNav: $(".carousel-arrows a"),
+	});
+}
